f7a5a1c2996d3407a916eb3b3d233d1e36bd97f6
dgiletto <dgiletto@udel.edu>
2025-09-23 17:13:13 -0400

    initial commit

---
+++ coreSync/src/main/java/com/dgiletto/coreSync/CoreSyncApplication.java
@@ -0,0 +1,13 @@
+package com.dgiletto.coreSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSyncApplication.class, args);
+	}
+
+}
---
+++ coreSync/src/test/java/com/dgiletto/coreSync/CoreSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgiletto.coreSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
