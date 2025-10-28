619a3dcbad33988f7a4e744f54296388cfa23029
dgiletto <dgiletto@udel.edu>
2025-10-28 16:58:00 -0400

    Allowing new vercel frontend domain in CORS

--- coreSync/src/main/java/com/dgiletto/coreSync/config/SecurityConfig.java
+++ coreSync/src/main/java/com/dgiletto/coreSync/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // Frontend URLs
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://core-sync-rouge.vercel.app"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
